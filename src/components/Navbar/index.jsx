24ebbc3dc49bc64b43a6073aa0042a15487f9e24
mohamed-amine-namasse <namasse.medamine@laplateforme.io>
2025-11-20 22:05:56 +0100

    commit

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,13 +1,13 @@
+// ...existing code...
 import React from "react";
 import Button from "react-bootstrap/Button";
 import Container from "react-bootstrap/Container";
 import Form from "react-bootstrap/Form";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
-import NavDropdown from "react-bootstrap/NavDropdown";
 import { PiShoppingCartFill } from "react-icons/pi";
 import { BiSolidUserCircle } from "react-icons/bi";
-import { NavLink } from "react-router-dom";
+import { NavLink } from "react-router";
 import { IoSearchOutline } from "react-icons/io5";
 import "./style.css";
 
@@ -16,19 +16,18 @@ function NavScrollExample() {
     <Navbar expand="lg" className="p-3">
       <Container
         fluid
-        className="d-flex align-items-center justify-content-between border border-warning "
+        className="d-flex align-items-center justify-content-between "
       >
-        <Navbar.Brand href="#" className="fw-bold">
+        <Navbar.Brand href="#" className="fw-bold ">
           Foot Market
         </Navbar.Brand>
 
-        {/* collapse contenant uniquement les liens */}
+        {/* toggler is direct child so we can reorder it with CSS on mobile */}
+        <Navbar.Toggle aria-controls="nav-links-collapse" />
+
+        {/* links inside collapse (toggler opens only these) */}
         <Navbar.Collapse id="nav-links-collapse">
-          <Nav
-            className="my-2 my-lg-0 "
-            style={{ maxHeight: "100px" }}
-            navbarScroll
-          >
+          <Nav className="my-2 my-lg-0 nav-links" navbarScroll>
             <Nav.Link as={NavLink} to="/" end>
               Accueil
             </Nav.Link>
@@ -41,17 +40,15 @@ function NavScrollExample() {
           </Nav>
         </Navbar.Collapse>
 
-        <Form
-          className="d-flex  mx-3 search-group align-items-center"
-          style={{ width: "40%", minWidth: "200px", maxWidth: "500px" }}
-        >
+        {/* search group: input hidden on < lg, only icon visible */}
+        <Form className="d-flex search-group align-items-center ">
           <Button
-            variant="outline-dark "
-            className="btn-search  d-flex align-items-center justify-content-center"
+            variant="outline-dark"
+            className="btn-search d-flex align-items-center justify-content-center"
+            aria-label="Recherche"
           >
             <IoSearchOutline size={25} />
           </Button>
-          {/* input masqué en mobile */}
           <Form.Control
             className="d-none d-lg-block"
             type="search"
@@ -60,24 +57,16 @@ function NavScrollExample() {
           />
         </Form>
 
-        <div className="d-flex align-items-center">
-          {/* le toggler ouvre uniquement #nav-links-collapse */}
-          <Navbar.Toggle aria-controls="nav-links-collapse" />
+        {/* icons remain outside collapse and visible in mobile */}
 
-          {/* icônes hors du collapse pour rester visibles en mobile */}
-          <Nav
-            className="my-2 my-lg-0 "
-            style={{ maxHeight: "100px" }}
-            navbarScroll
-          >
-            <Nav.Link href="#action1">
-              <PiShoppingCartFill size={30} />
-            </Nav.Link>
-            <Nav.Link href="#action2">
-              <BiSolidUserCircle size={30} />
-            </Nav.Link>
-          </Nav>
-        </div>
+        <Nav className="nav-icons d-flex align-items-center ">
+          <Nav.Link href="#cart" className="p-1">
+            <PiShoppingCartFill size={30} />
+          </Nav.Link>
+          <Nav.Link href="#user" className="p-1">
+            <BiSolidUserCircle size={30} />
+          </Nav.Link>
+        </Nav>
       </Container>
     </Navbar>
   );

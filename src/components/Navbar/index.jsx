e42d0f1dd614c84a2e1f03701591eca18a284bb4
mohamed-amine-namasse <namasse.medamine@laplateforme.io>
2025-11-20 13:31:23 +0100

    commit

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -4,45 +4,70 @@ import Form from "react-bootstrap/Form";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
-
+import { PiShoppingCartFill } from "react-icons/pi";
+import { BiSolidUserCircle } from "react-icons/bi";
+import { NavLink } from "react-router";
+import { IoSearchOutline } from "react-icons/io5";
+import "./style.css";
 function NavScrollExample() {
   return (
-    <Navbar expand="lg" className="bg-body-tertiary">
-      <Container fluid>
-        <Navbar.Brand href="#">Navbar scroll</Navbar.Brand>
-        <Navbar.Toggle aria-controls="navbarScroll" />
-        <Navbar.Collapse id="navbarScroll">
-          <Nav
-            className="me-auto my-2 my-lg-0"
-            style={{ maxHeight: "100px" }}
-            navbarScroll
+    <Navbar expand="lg" className="p-3">
+      <Container
+        fluid
+        className="d-flex align-items-center justify-content-between "
+      >
+        <Navbar.Brand href="#" className="fw-bold">
+          Foot Market
+        </Navbar.Brand>
+        <Nav
+          className="my-2 my-lg-0 "
+          style={{ maxHeight: "100px" }}
+          navbarScroll
+        >
+          <Nav.Link as={NavLink} to="/" end>
+            Accueil
+          </Nav.Link>
+          <Nav.Link as={NavLink} to="/shop">
+            Boutique
+          </Nav.Link>
+          <Nav.Link as={NavLink} to="/register">
+            Inscription
+          </Nav.Link>
+        </Nav>
+        <Form
+          className="d-flex  mx-3 search-group align-items-center"
+          style={{ width: "40%", minWidth: "200px", maxWidth: "500px" }}
+        >
+          <Button
+            variant="outline-dark"
+            className="btn-search  d-flex align-items-center justify-content-center"
           >
-            <Nav.Link href="#action1">Home</Nav.Link>
-            <Nav.Link href="#action2">Link</Nav.Link>
-            <NavDropdown title="Link" id="navbarScrollingDropdown">
-              <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action4">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action5">
-                Something else here
-              </NavDropdown.Item>
-            </NavDropdown>
-            <Nav.Link href="#" disabled>
-              Link
-            </Nav.Link>
-          </Nav>
-          <Form className="d-flex">
-            <Form.Control
-              type="search"
-              placeholder="Search"
-              className="me-2"
-              aria-label="Search"
-            />
-            <Button variant="outline-success">Search</Button>
-          </Form>
-        </Navbar.Collapse>
+            <IoSearchOutline />
+          </Button>
+          <Form.Control
+            type="search"
+            placeholder="Rechercher un article"
+            aria-label="Search"
+          />
+        </Form>
+
+        <div className="d-flex align-items-center">
+          <Navbar.Toggle aria-controls="navbarScroll" />
+          <Navbar.Collapse id="navbarScroll">
+            <Nav
+              className="my-2 my-lg-0 "
+              style={{ maxHeight: "100px" }}
+              navbarScroll
+            >
+              <Nav.Link href="#action1">
+                <PiShoppingCartFill size={30} />
+              </Nav.Link>
+              <Nav.Link href="#action2">
+                <BiSolidUserCircle size={30} />
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </div>
       </Container>
     </Navbar>
   );

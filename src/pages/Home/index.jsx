3f7448648d33beb76bff47654a4d04b164ebef63
johnwickone <165157833+johnwickone@users.noreply.github.com>
2025-11-20 15:52:52 +0100

    josué-premier

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,11 +1,14 @@
 import "./style.css";
+import MonCarousel from "../../components/carousel";  
 
 function Home() {
   return (
     <div>
       <p>ICI TA PAGE DACCUEIL!</p>
+      <MonCarousel />
     </div>
   );
 }
 
 export default Home;
+

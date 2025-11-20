8131e86208e16fefdcaf3379fc0b462ab2f00be6
Elies75013 <ton-email-githubexample.com>
2025-11-20 15:51:58 +0100

    Page a propos et erreur 404

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router";
-import Contact from "./pages/Contact";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Contact from "./pages/Contact/index.jsx";
+import About from "./pages/about/index.jsx";
+import NotFound from "./pages/notfound/index.jsx";
 import './App.css';
+
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/contact" element={<Contact />} />
+        <Route path="/about" element={<About />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </Router>
   );

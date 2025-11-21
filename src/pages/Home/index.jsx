ac648e3d7294b5e9c038bb91f906efeeb58500b3
johnwickone <165157833+johnwickone@users.noreply.github.com>
2025-11-21 16:09:24 +0100

    3eme

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,14 +1,34 @@
+// ...existing code...
 import "./style.css";
-import MonCarousel from "../../components/carousel";  
+import MonCarousel from "../../components/MonCarousel";
+import Cards from "../../components/Cards";
+
+
+const API_URL =
+  "https://mohamed-amine-namasse.students-laplateforme.io/wordpress-eco/wordpress/wp-json/wp/v2/posts";
+
+
 
 function Home() {
   return (
     <div>
-      <p>ICI TACUEIL!</p>
+      <header className="home-header">
+        <h1 className="display-4 text-center">Nouvelle collection</h1>
+        <p className="lead text-center">Autonne & Hiver</p>
+      </header>
+
       <MonCarousel />
+      <Cards />
+
+      <section className="container mt-5">
+        <h2 className="h4">Nouveautés cette semaine</h2>
+        <p>Nos crampons du moments.</p>
+      </section>
     </div>
   );
 }
 
 export default Home;
+// ...existing code...
+
 

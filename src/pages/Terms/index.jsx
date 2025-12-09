2ede30a6f409be8ad68918e8797a67c07087f5ad
mohamed-amine-namasse <namasse.medamine@laplateforme.io>
2025-12-09 15:47:05 +0100

    c

--- src/pages/Terms/index.jsx
+++ src/pages/Terms/index.jsx
@@ -16,41 +16,12 @@ function Terms() {
         utilisateur de votre navigateur sont collectés pour nous aider à la
         détection des commentaires indésirables.
       </p>
-      <h3>Cookies</h3>
-      <p>
-        Si vous déposez un commentaire sur notre site, il vous sera proposé
-        d’enregistrer votre nom, adresse e-mail et site dans des cookies. C’est
-        uniquement pour votre confort afin de ne pas avoir à saisir ces
-        informations si vous déposez un autre commentaire plus tard. Ces cookies
-        expirent au bout d’un an.
-      </p>
-      <p>
-        Si vous vous rendez sur la page de connexion, un cookie temporaire sera
-        créé afin de déterminer si votre navigateur accepte les cookies. Il ne
-        contient pas de données personnelles et sera supprimé automatiquement à
-        la fermeture de votre navigateur.
-      </p>
-      <p>
-        Lorsque vous vous connecterez, nous mettrons en place un certain nombre
-        de cookies pour enregistrer vos informations de connexion et vos
-        préférences d’écran. La durée de vie d’un cookie de connexion est de
-        deux jours, celle d’un cookie d’option d’écran est d’un an. Si vous
-        cochez « Se souvenir de moi », votre cookie de connexion sera conservé
-        pendant deux semaines. Si vous vous déconnectez de votre compte, le
-        cookie de connexion sera effacé.
-      </p>
-      <p>
-        En modifiant ou en publiant une publication, un cookie supplémentaire
-        sera enregistré dans votre navigateur. Ce cookie ne comprend aucune
-        donnée personnelle. Il indique simplement l’ID de la publication
-        modifiée. Il expire au bout d’un jour.
-      </p>
+
       <h3>Contenu embarqué depuis d’autres sites</h3>
       <p>
-        Les articles de ce site peuvent inclure des contenus intégrés (par
-        exemple des vidéos, images, articles…). Le contenu intégré depuis
-        d’autres sites se comporte de la même manière que si le visiteur se
-        rendait sur cet autre site.
+        Ce site peut inclure des contenus intégrés (par exemple des vidéos,
+        images, articles…). Le contenu intégré depuis d’autres sites se comporte
+        de la même manière que si le visiteur se rendait sur cet autre site.
       </p>
       <p>
         Ces sites web pourraient collecter des données sur vous, utiliser des
